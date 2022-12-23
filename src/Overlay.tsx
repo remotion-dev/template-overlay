d33851c380dce1a5408305ac3bab6f8d27df1f73
Jonny Burger <jonathanburger11@gmail.com>
2022-12-23 15:53:07 +0100

    cleanup template

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -5,56 +5,45 @@ import {
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import {useCallback} from 'react';
 import React, {useMemo} from 'react';
-import {loadFont} from '@remotion/google-fonts/OpenSans';
+import {loadFont} from '@remotion/google-fonts/Roboto';
 
 const {fontFamily} = loadFont();
-const backgroundStyle: React.CSSProperties = {
-	background: ` linear-gradient(45deg, rgba(0, 0, 0, 0.1) 25%, transparent 25%),
-	linear-gradient(135deg, rgba(0, 0, 0, 0.1) 25%, transparent 25%),
-	linear-gradient(45deg, transparent 75%, rgba(0, 0, 0, 0.1) 75%),
-	linear-gradient(135deg, transparent 75%, rgba(0, 0, 0, 0.1) 75%), white`,
-	backgroundSize: '49px 49px',
-	backgroundPosition: '0 0, 24.5px 0, 24.5px -24.5px, 0px 24.5px',
-};
 
 const title: React.CSSProperties = {
 	fontFamily,
-	fontSize: '5em',
-	color: '#000',
+	fontSize: 80,
 	fontWeight: 'bold',
 };
 
 const text: React.CSSProperties = {
 	fontWeight: 'bold',
 	fontFamily,
-	fontSize: '2.5em',
+	fontSize: 40,
 	color: '#4290F5',
 };
+
+const disappearBeforeEnd = 20;
 
 export const Overlay: React.FC = () => {
 	const frame = useCurrentFrame();
 	const {fps, durationInFrames} = useVideoConfig();
 
-	const entry = useCallback(
-		(delay: number) =>
-			spring({
-				fps,
-				frame: frame - delay,
-				config: {
-					mass: 0.5,
-				},
-			}),
-		[fps, frame]
-	);
+	const scale = spring({
+		fps,
+		frame,
+		config: {
+			mass: 0.5,
+		},
+	});
 
 	const out = spring({
 		fps,
-		frame: frame - durationInFrames + 20,
+		frame: frame - durationInFrames + disappearBeforeEnd,
 		config: {
 			damping: 200,
 		},
+		durationInFrames: disappearBeforeEnd,
 	});
 
 	const rotate = interpolate(out, [0, 1], [0, -Math.PI / 20]);
@@ -67,14 +56,15 @@ export const Overlay: React.FC = () => {
 			borderRadius: 25,
 			right: 90,
 			top: 90,
-			transform: `scale(${entry(
-				0
-			)})  translateY(${outY}px) rotate(${rotate}rad)`,
+			scale: String(scale),
+			translate: `0 ${outY}px`,
+			rotate: `${rotate}rad`,
 			padding: 40,
 		};
-	}, [entry, outY, rotate]);
+	}, [scale, outY, rotate]);
+
 	return (
-		<AbsoluteFill style={backgroundStyle}>
+		<AbsoluteFill>
 			<div style={container}>
 				<div style={title}>Look</div>
 				<div style={text}>I'm an overlay!</div>
