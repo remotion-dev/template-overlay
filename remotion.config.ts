e1f75fd0f9b648bafbd112a1cc3ce2388216f750
Jonny Burger <jonathanburger11@gmail.com>
2022-12-23 16:58:13 +0100

    make muted

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,5 +1,7 @@
 import {Config} from 'remotion';
+
 Config.Rendering.setImageFormat('png');
 Config.Output.setPixelFormat('yuva444p10le');
 Config.Output.setCodec('prores');
 Config.Output.setProResProfile('4444');
+Config.Rendering.setMuted(true);
