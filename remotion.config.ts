a46b868c9b17619b4cae78007d8e871f5d8ac83a
Jonny Burger <jonathanburger11@gmail.com>
2024-04-27 10:25:15 +0200

    upgrade and use prettier defaults

--- remotion.config.ts
+++ remotion.config.ts
@@ -4,10 +4,10 @@
 // Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 // All configuration options: https://remotion.dev/docs/config
 
-import {Config} from '@remotion/cli/config';
+import { Config } from "@remotion/cli/config";
 
-Config.setVideoImageFormat('png');
-Config.setPixelFormat('yuva444p10le');
-Config.setCodec('prores');
-Config.setProResProfile('4444');
+Config.setVideoImageFormat("png");
+Config.setPixelFormat("yuva444p10le");
+Config.setCodec("prores");
+Config.setProResProfile("4444");
 Config.setMuted(true);
